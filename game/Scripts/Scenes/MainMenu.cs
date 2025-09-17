4fd6529669d9562c54606b04c96a2d3aa5823607
Ruz <roseller.mortera@gmail.com>
2025-09-17 16:10:50 +0800

    Set up animations for transition in main menu

--- game/Scripts/Scenes/MainMenu.cs
+++ game/Scripts/Scenes/MainMenu.cs
@@ -5,9 +5,10 @@ public partial class MainMenu : Control
 {
     // okay so for some reason I need to do 3 animation players for the 3 various sets so *fuck me then* lmao, this'll be funny
     // to whomever that decompiles this later, hi Cedric.
-    [Export] private AnimationPlayer LettersDrifting; 
-    [Export] private AnimationPlayer Starfield; 
+    [Export] private AnimationPlayer LettersDrifting;
+    [Export] private AnimationPlayer Starfield;
     [Export] private AnimationPlayer Spaceship;
+    [Export] private AnimationPlayer CameraAnimations;
 
 
     public override void _Ready()
@@ -17,5 +18,18 @@ public partial class MainMenu : Control
         Spaceship.Play("spaceshipLoop");
         // Yes the names are inconsistent, bite me i was tired (this is in the event i forget to change them proper later lmao)
         //TO DO: make names consistent, maybe also try to find a way to get these 3 animations to play from 1 player for efficiency
+    }
+
+    private void _on_play_pressed()
+    {
+        CameraAnimations.Play("MoonTransition");
+    }
+
+    private void _on_camera_animations_animation_finished(string anim_name)
+    {
+        if (anim_name == "MoonTransition")
+        {
+            GetTree().ChangeSceneToFile("res://Scenes/Levels/europa.tscn");
+        }
     }
 }
