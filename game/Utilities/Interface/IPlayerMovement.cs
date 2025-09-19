4cf8c558c816f0795d3929ac309a4a1a99cadca7
Ruz <roseller.mortera@gmail.com>
2025-09-19 20:40:18 +0800

    added player movement interface

---
+++ game/Utilities/Interface/IPlayerMovement.cs
@@ -0,0 +1,8 @@
+public interface IPLayerMovement
+{
+    /*
+    Interface method for handling player movement, the reason I made this interface is because I (currently) have 2 versions of player movement
+    that being the platformer movement and the zero-g movement for the flying state.
+    */
+    void HandleMovement(float delta); 
+}
