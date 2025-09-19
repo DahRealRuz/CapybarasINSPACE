110cef9d439bd1f27a968f2a7367d613f9ed7dc0
Ruz <roseller.mortera@gmail.com>
2025-09-19 21:04:09 +0800

    deleted the initial basic files lmao, then added some folders woo

--- game/Utilities/Interface/IPlayerMovement.cs
+++
@@ -1,8 +0,0 @@
-public interface IPlayerMovement
-{
-    /*
-    Interface method for handling player movement, the reason I made this interface is because I (currently) have 2 versions of player movement
-    that being the platformer movement and the zero-g movement for the flying state.
-    */
-    public void HandleMovement(float delta); 
-}
